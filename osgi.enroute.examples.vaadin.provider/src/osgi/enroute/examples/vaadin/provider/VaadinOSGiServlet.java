8d00b4326ca732cdfdfbf99891faf1a1b7ecb4a7
Peter Kriens <peter.kriens@aqute.biz>
2016-06-07 12:51:32 +0200

    The servlet is not a component, it is registered manually

--- osgi.enroute.examples.vaadin.provider/src/osgi/enroute/examples/vaadin/provider/VaadinOSGiServlet.java
+++ osgi.enroute.examples.vaadin.provider/src/osgi/enroute/examples/vaadin/provider/VaadinOSGiServlet.java
@@ -2,13 +2,7 @@ package osgi.enroute.examples.vaadin.provider;
 
 import java.util.Properties;
 
-import javax.servlet.Servlet;
-
-import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceReference;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ConfigurationPolicy;
-import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.DefaultDeploymentConfiguration;
@@ -21,11 +15,6 @@ import com.vaadin.ui.UI;
 import osgi.enroute.examples.vaadin.api.Application;
 
 @SuppressWarnings("serial")
-@Component(name = "osgi.enroute.vaadin.servlet", //
-		property = {
-
-				HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN + "=/foo", Constants.SERVICE_RANKING
-						+ ":Integer=100" }, service = Servlet.class, configurationPolicy = ConfigurationPolicy.OPTIONAL, immediate = true)
 @VaadinServletConfiguration(productionMode = false, ui = UI.class)
 public class VaadinOSGiServlet extends VaadinServlet {
 
